34fdaede2dc5b884a00d4c3977ee97988d55bed2
Infinidoge <infinidoge@doge-inc.net>
2023-02-01 17:04:43 -0500

    complete Project Part II Section 4

--- kernel/04_scroll/kernel/kernel.c
+++ kernel/04_scroll/kernel/kernel.c
@@ -6,12 +6,17 @@ void _start() {
 
     /* Fill up the screen */
     int i = 0;
-    for (i = 0; i < 24; i++) {
+    for (i = 0; i < 30; i++) {
         char str[255];
         int_to_ascii(i, str);
         kprint_at(str, 0, i);
+        for (int j = 0; j < 100000000; j++) {
+            ;
+        }
     }
 
-    kprint_at("This text forces the kernel to scroll. Row 0 will disappear. ", 60, 24);
-    kprint("And with this text, the kernel will scroll again, and row 1 will disappear too!");
+    kprint_at("This text forces the kernel to scroll. Row 0 will disappear. ",
+        60, 24);
+    kprint("And with this text, the kernel will scroll again, and row 1 will "
+           "disappear too!");
 }
