cc32c0da6f2016d9b51f2b5f34b39f3bb29aaa85
Infinidoge <infinidoge@doge-inc.net>
2023-02-06 18:56:36 -0500

    skip trying harddisc in QEMU boot

--- kernel/run.nix
+++ kernel/run.nix
@@ -2,34 +2,34 @@
 let
   tmp = "tmp=$(mktemp -d --tmpdir cs3502-kernel.XXXX); cd $tmp";
   i386 = "${qemu}/bin//qemu-system-i386";
-  qemuFile = "-drive file=os-image.bin,index=0,if=floppy,format=raw";
+  qemuArgs = "-boot order=a -drive file=os-image.bin,index=0,if=floppy,format=raw";
 in
 {
   run = writeScriptBin "run" ''
     ${tmp}
     cp ${drv}/os-image.bin .
     chmod u+rwx os-image.bin
-    ${i386} -vga std ${qemuFile}
+    ${i386} -vga std ${qemuArgs}
   '';
 
   curses = writeScriptBin "curses" ''
     ${tmp}
     cp ${drv}/os-image.bin .
     chmod u+rwx os-image.bin
-    ${i386} -display curses ${qemuFile}
+    ${i386} -display curses ${qemuArgs}
   '';
 
   debug = writeScriptBin "debug" ''
     ${tmp}
     cp ${drv}/os-image.bin ${drv}/kernel.elf .
     chmod u+rwx os-image.bin
-    ${i386} -display curses -s ${qemuFile}
+    ${i386} -display curses -s ${qemuArgs}
   '';
 
   gdb = writeScriptBin "gdb" ''
     ${tmp}
     cp ${drv}/kernel.elf .
     cp -r ${drv.src}/* .
-    ${lib.getExe gdb} -ex "target remote localhost:1234" -ex "symbol-file kernel.elf"
+    ${gdb}/bin/i386-elf-gdb -ex "target remote localhost:1234" -ex "symbol-file kernel.elf"
   '';
 }
