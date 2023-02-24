5021ebbad51c82c47c5402193fa38b5aee3894cc
Infinidoge <infinidoge@doge-inc.net>
2023-02-24 14:57:13 -0500

    WIP: add strbegins with; make strlen/strcmp const

--- kernel/09_memory/libc/string.c
+++ kernel/09_memory/libc/string.c
@@ -55,7 +55,7 @@ void reverse(char s[]) {
 }
 
 /* K&R */
-int strlen(char s[]) {
+int strlen(const char s[]) {
     int i = 0;
     while (s[i] != '\0')
         ++i;
@@ -75,11 +75,23 @@ void backspace(char s[]) {
 
 /* K&R
  * Returns <0 if s1<s2, 0 if s1==s2, >0 if s1>s2 */
-int strcmp(char s1[], char s2[]) {
+int strcmp(const char s1[], const char s2[]) {
     int i;
     for (i = 0; s1[i] == s2[i]; i++) {
         if (s1[i] == '\0')
             return 0;
     }
     return s1[i] - s2[i];
+}
+
+bool strbeginswith(const char s1[], const char s2[], int *rest) {
+    int i;
+    for (i = 0; s2[i] != '\0'; i++) {
+        if (s1[i] != s2[i])
+            return false;
+    }
+
+    *rest = i;
+
+    return true;
 }
--- kernel/09_memory/libc/string.h
+++ kernel/09_memory/libc/string.h
@@ -6,9 +6,10 @@
 void int_to_ascii(int n, char str[]);
 void hex_to_ascii(int n, char str[]);
 void reverse(char s[]);
-int strlen(char s[]);
+int strlen(const char s[]);
 void backspace(char s[]);
 void append(char s[], char n);
-int strcmp(char s1[], char s2[]);
+int strcmp(const char s1[], const char s2[]);
+bool strbeginswith(const char s1[], const char s2[], int *rest);
 
 #endif
