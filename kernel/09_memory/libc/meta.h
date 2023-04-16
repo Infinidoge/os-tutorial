9cdc9befd0d962b4087ef85b6af3bc59166fbb1e
Infinidoge <infinidoge@inx.moe>
2023-04-16 02:58:37 -0400

    add comments explaining linker-provided values

--- kernel/09_memory/libc/meta.h
+++ kernel/09_memory/libc/meta.h
@@ -3,10 +3,13 @@
 
 #include <stddef.h>
 
+// Linker constants
+// (Actually are pointers)
 extern size_t _etext;
 extern size_t _edata;
 extern size_t _end;
 
+// Type-converted versions
 #define ETEXT (size_t) & _etext
 #define EDATA (size_t) & _edata
 #define END (size_t) & _end
