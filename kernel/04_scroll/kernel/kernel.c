c11c6536044db8c9a13ecf5b0af5c8fe2b8ee269
Infinidoge <infinidoge@inx.moe>
2023-04-24 01:17:52 -0400

    04_scroll: fix printing errors, reduce delay, reformat

--- kernel/04_scroll/kernel/kernel.c
+++ kernel/04_scroll/kernel/kernel.c
@@ -6,17 +6,16 @@ void _start() {
 
     /* Fill up the screen */
     int i = 0;
-    for (i = 0; i < 30; i++) {
+    for (i = 0; i < 25; i++) {
         char str[255];
         int_to_ascii(i, str);
         kprint_at(str, 0, i);
-        for (int j = 0; j < 100000000; j++) {
+        for (int j = 0; j < 50000000; j++) {
             ;
         }
     }
 
-    kprint_at("This text forces the kernel to scroll. Row 0 will disappear. ",
-        60, 24);
+    kprint_at("This text forces the kernel to scroll. Row 0 will disappear. ", 60, 24);
     kprint("And with this text, the kernel will scroll again, and row 1 will "
            "disappear too!");
 }
