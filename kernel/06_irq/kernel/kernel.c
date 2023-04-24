b15c5a5433645c087fce0d2a143a08311fd3f568
Infinidoge <infinidoge@inx.moe>
2023-04-24 01:18:11 -0400

    06_irq: move irq_demonstration out of _start
    Avoids nesting functions, which is a GCC language extension, and not
    supported by all compilers (e.g., Clang)

--- kernel/06_irq/kernel/kernel.c
+++ kernel/06_irq/kernel/kernel.c
@@ -3,13 +3,13 @@
 #include "../drivers/screen.h"
 #include "util.h"
 
+void irq_demonstration(registers_t r) {
+    kprint("Called from an IRQ demonstration function!");
+}
+
 void _start() {
     isr_install();
     /* Test the interrupts */
-
-    void irq_demonstration(registers_t r) {
-        kprint("Called from an IRQ demonstration function!");
-    }
     register_interrupt_handler(IRQ0, irq_demonstration);
 
     asm volatile("int $32");
