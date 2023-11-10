036706c69d924178d48970d896ba3d093572c506
Infinidoge <infinidoge@inx.moe>
2023-11-09 21:27:42 -0500

    cpu/types: add bit extraction macros

--- kernel/09_memory/cpu/types.h
+++ kernel/09_memory/cpu/types.h
@@ -10,4 +10,9 @@
 #define low_16(address) (uint16_t)((address)&0xFFFF)
 #define high_16(address) (uint16_t)(((address) >> 16) & 0xFFFF)
 
+#define BIT(var, pos) (bool)(((var) >> (pos)) & 1)
+#define NIBBLE(var, pos) (uint8_t)(((var) >> (pos)) & 0xF)
+#define BYTE(var, pos) (uint8_t)(((var) >> (pos)) & 0xFF)
+#define WORD(var, pos) (uint16_t)(((var) >> (pos)) & 0xFFFF)
+
 #endif
