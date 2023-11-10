6b01b1a411d8d15e9bae6caaa660cdd071255f1d
Infinidoge <infinidoge@inx.moe>
2023-11-09 21:28:52 -0500

    libc/linkedlist: add WIP linked list implementation

--- kernel/09_memory/libc/linkedlist.h
+++ kernel/09_memory/libc/linkedlist.h
@@ -1,6 +1,12 @@
 #ifndef LINKEDLIST_
 #define LINKEDLIST_
 
+typedef struct Node {
+    void *data;
+    struct Node *prev;
+    struct Node *next;
+} node;
+
 #define NEXT(node) node = node->next
 #define PREV(node) node = node->prev
 
